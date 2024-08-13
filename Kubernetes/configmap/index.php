1f3cac9fd66761f4045055f94320197eb634c517
A <And13@mail.ru>
2024-08-13 17:32:25 +0300

    configmap

---
+++ Kubernetes/configmap/index.php
@@ -0,0 +1,8 @@
+    <?php
+    $ip_server = $_SERVER['SERVER_ADDR'];
+        
+    echo "<h1>Hello from Kubernetes SECRETS</h1><br>";
+
+    echo "the $enviroment  IP address is: $ip_server", "<br><p>"
+
+    ?>
